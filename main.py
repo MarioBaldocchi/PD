d3450982890f2f3a4f4484d6414df0b3824cdc49
Artem Vartanov <artemvar@ucm.es>
2024-02-26 17:51:10 +0100

    Añadida la descomposicion en horas de datos de la boya

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from extraccion_datos import extraccion_datos_boya
 from extraccion_datos import extraccion_datos_clima
 from transformacion_datos import descomponerTiempo
+from transformacion_datos import descomponerHoras
 import json
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -9,6 +10,7 @@ from datetime import datetime
 
 
 df_principal = extraccion_datos_boya()
+print(descomponerHoras(df_principal))
 df_secundario = extraccion_datos_clima("01/01/2022", "31/01/2022")
 df_secundario = descomponerTiempo(df_secundario)
 df_secundario
--- transformacion_datos.py
+++ transformacion_datos.py
@@ -9,3 +9,31 @@ def descomponerTiempo(df):
     df.drop('valid_time_gmt', axis=1, inplace=True)
     df.drop('expire_time_gmt', axis=1, inplace=True)
     return df
+
+
+def descomponerHoras(df):
+    """Transforma columnas con los datos horarios a filas con columna hora"""
+    new_df = None # dataframe separados por horas
+
+    for h in range(0, 24):
+        hora = str(h).zfill(2) # necesitamos horas de dos digitos
+        separate_df = columnasAFilas(df, hora)
+        if new_df is None:
+            new_df = separate_df
+        else:
+            new_df = pd.concat([new_df, separate_df]) # unimos con el dataframe anterior
+
+    return new_df
+
+
+def columnasAFilas(df, hora):
+    "Saca los datos para una hora concreta(columnas que acaban por esa hora)"
+    selected_cols = df.columns[df.columns.str.endswith(hora)] # columnas que acaban en esa hora
+    # Sacamos las columnas de la hora actual
+    separate_df = df.loc[:][selected_cols]
+    # Renombramos las columnas (quitamos hora del fin)
+    cols_sin_hora = selected_cols.str.slice(stop=-2)
+    separate_df = separate_df.rename(dict(zip(separate_df.columns, cols_sin_hora)), axis=1)
+    separate_df["hora"] = hora
+
+    return separate_df
