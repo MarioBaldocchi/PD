2adf4dda5febbf1f2e48b126bfb071dc4644b5c2
MarioBaldocchi <marbaldo@ucm.es>
2024-04-03 16:36:48 +0200

    Datos separados en train y test y comienzo de modelos

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 import pandas as pd
+
 from exploracion.exploracion_datos import *
-from sklearn.model_selection import train_test_split
 
 df_def = pd.read_parquet("./clean/df_definitivo.parquet")
-l = df_def.columns
-df_def = df_def[['anio', 'mes', 'dia', 'hora', 'temp', 'dewPt', 'heat_index', 'rh',
-       'pressure', 'vis', 'wc', 'wspd', 'feels_like', 'uv_index', 'wdir_E',
-       'wdir_ENE', 'wdir_ESE', 'wdir_N', 'wdir_NE', 'wdir_NNE', 'wdir_NNW',
-       'wdir_NW', 'wdir_S', 'wdir_SE', 'wdir_SSE', 'wdir_SSW', 'wdir_SW',
-       'wdir_VAR', 'wdir_W', 'wdir_WNW', 'wdir_WSW', 'Vviento',
-       'PeriodoOlas', 'Temperatura', 'Lluvia', 'Nubosidad', 'AlturaOlas']]
 
 '''Ahora vamos a realizar un breve análisis exploratorio para visualizar mejor los datos y 
 poder entender mejor el proceso de predicción de la siguiente práctica '''
@@ -24,9 +17,7 @@ poder entender mejor el proceso de predicción de la siguiente práctica '''
 #correlaciones(df_def[['temp', 'dewPt', 'heat_index', 'rh','pressure', 'vis', 'wc', 'wspd', 'feels_like', 'uv_index',  'Vviento', 'AlturaOlas','PeriodoOlas', 'Temperatura', 'Lluvia', 'Nubosidad']], 'spearman')
 
 # medias y desviaciones de dummies
-#analisis_dummies(df_def, 'wdir_')
+analisis_dummies(df_def, 'wdir_')
 
 #Separamos en train y test
-target = df_def.iloc[:, -1]
-data = df_def.iloc[:, :-1]
-X_train, X_test, y_train, y_test = train_test_split(data, target, train_size=0.77)
+X_train, X_test, y_train, y_test = train_test_split(df_def.drop(columns=['AlturaOlas']), df_def.AlturaOlas, train_size=0.77)
