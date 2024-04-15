ea8cc77da318b01ca158b72a64741d073344303e
Artem Vartanov <artemvar@ucm.es>
2024-04-16 00:34:58 +0200

    Mlflow sacado de la carpeta

--- modelos/__init__.py
+++ modelos/__init__.py
@@ -2,4 +2,4 @@ RANDOM_SEED = 777
 
 from modelos.metric_utils import calcular_metricas_search, calcular_metricas
 from modelos.subsets_manager import datos_full, sep_train_test, cv_folds
-from modelos.mlflow.ml_flow_utils import MLFlow
+from modelos.ml_flow_utils import MLFlow
