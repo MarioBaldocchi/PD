3d1351234b816dc7caec1f6ebc9621111bea8d9f
MarioBaldocchi <marbaldo@ucm.es>
2024-05-06 13:29:47 +0200

    notebook heuristica casi terminado

--- modelos/subsets_manager.py
+++ modelos/subsets_manager.py
@@ -15,7 +15,7 @@ Devuelve el conjunto de datos completo:
 matriz de variables explicativas, matriz de variable respuesta
 '''
 def datos_full():
-    df = pd.read_parquet("../../clean/df_definitivo.parquet")
+    df = pd.read_parquet("../clean/df_definitivo.parquet")
     target = "AlturaOlas"
     X = df.drop(columns=target)
     y = df[target]
@@ -35,6 +35,7 @@ Devuelve el scaler
 '''
 def get_scaler():
     X_train, X_test, y_train, y_test = sep_train_test()
+    X_train = X_train.drop(columns=["Temperatura", 'wspd', 'anio', 'mes', 'dia', 'hora'])
     scaler = ColumnTransformer(
         transformers=[
             # no escalonamos las dummy de dirección de viento
